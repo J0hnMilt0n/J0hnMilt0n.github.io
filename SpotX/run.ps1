35ea9f35f4a8f50ba78f3e8b0996b658a09e5d99
John Milton M <155903304+J0hnMilt0n@users.noreply.github.com>
2024-02-18 21:56:06 +0530

    Update run.ps1

--- SpotX/run.ps1
+++ SpotX/run.ps1
@@ -247,7 +247,7 @@ function Format-LanguageCode {
             $returnCode = 'sk'
             break
         }
-        '^(ta|ta_IN|ta_MY|ta_SG|ta_LK)' {
+        '^(ta|ta-IN|ta-MY|ta-SG|ta-LK)' {
             $returnCode = 'ta'
             break
         }
